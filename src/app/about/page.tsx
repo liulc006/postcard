abfcdd5238af956719d94ae38e1c9d97a635d97f
Luca Liu <lucaliu96@gmail.com>
2026-02-22 22:48:11 -0500

    added simple about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,17 @@
+import { Box, Typography } from "@mui/material";
+
 export default function About() {
-    const aboutText: string = "This is the About page of the Postcard App. Here you can find information about the app and its features.";
     
     return (
-        <h1>{aboutText}</h1>
+        <Box sx={{flex:1,display:'flex', flexDirection:'row', margin:'0 5rem 0 5rem', justifyContent:'center', alignItems:'center'}}>
+            <Box sx={{marginRight:'3rem'}}>
+                <img src='/images/Luca-cartoon.png' alt="Luca's Cartoon" width="200" height="200" style={{border:'1px solid black', borderRadius:'50%', objectFit:'cover'}}/>
+            </Box>
+            <Box>
+                <Typography sx={{color:'var(--color-primary)'}}>Hi, I am Luca, software engineer that developed this app.</Typography>
+                <Typography sx={{color:'var(--color-primary)', marginTop: 2}}>Thank you for visiting the Postcard App!</Typography>
+                <Typography sx={{color:'var(--color-primary)', marginTop: 2}}>Checkout my Profile: <a href="https://portfolio-byzk.onrender.com/">Portfolio Link</a></Typography>
+            </Box>
+        </Box>
     )
 }
